adb432c96f02f9d60787e702965d5f832dec0f3b
Mylenbro <makinoog@gmail.com>
2023-03-28 14:56:24 -0300

    feat: add script that starts the server

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import 'dotenv/config'
+import express from 'express';
+import { ParseServer } from 'parse-server';
+import ParseDashboard from 'parse-dashboard'
+
+const databaseURI = process.env.MONGODB_URI
+if (!databaseURI) {
+    throw 'Enviroment variable MONGODB_URI not found.'
+}
+
+const dev = process.env.DEV === '0' ? false : true
+const appId = process.env.APP_ID ?? 'appId'
+const masterKey = process.env.MASTER_KEY ?? 'masterKey'
+const serverURL = process.env.SERVER_URL ?? "http://localhost:1337"
+const user = process.env.USER ?? 'admin'
+const pass = process.env.PASS ?? 'admin'
+const port = process.env.PORT ?? 1337
+
+const server = new ParseServer({
+    appId: appId,
+    masterKey: masterKey,
+    databaseURI: databaseURI,
+    allowExpiredAuthDataToken: false,
+    allowClientClassCreation: false
+});
+
+console.log('Starting parse-server . . .')
+await server.start()
+
+const dashboard = new ParseDashboard({
+    apps:
+        [{
+            appName: "myApplication",
+            appId: appId,
+            masterKey: masterKey,
+            serverURL: serverURL + '/parse',
+            production: !dev
+        }],
+    users:
+        [{
+            user: user,
+            pass: pass,
+            apps: [{ appId: appId }]
+        }],
+    trustProxy: 1
+}, {
+    allowInsecureHTTP: false
+});
+
+const app = express();
+app.use('/parse', server.app);
+app.use('/', dashboard);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
